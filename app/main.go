88964ef237f7f18f0d17ad8b7aecb710f2ca8b01
sako0 <y.m1911a1.0921@gmail.com>
2021-12-30 00:17:05 +0900

    直接指定

--- app/main.go
+++ app/main.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"log"
 	"net/http"
-	"os"
 
 	"codetest-docker/app/controllers"
 
@@ -14,15 +13,16 @@ import (
 
 func main() {
 	// github actionsではdbがコンテナではないので、環境変数で指定しなおす
-	db, err := sql.Open("mysql", "root@tcp("+os.Getenv("DB_HOST")+")/codetest")
+	db, err := sql.Open("mysql", "root@tcp(127.0.0.1)/codetest")
 	if err != nil {
 		log.Fatal(err)
 	}
-	controller := controllers.Controller{}
 	router := mux.NewRouter()
+	controller := controllers.Controller{}
 	router.HandleFunc("/users", controller.GetUsers(db)).Methods("GET")
 	router.HandleFunc("/transactions", controller.GetTransactions(db)).Methods("GET")
 	router.HandleFunc("/transactions", controller.AddTransaction(db)).Methods("POST")
+
 	log.Println("Server up on port 8888...")
 	log.Fatal(http.ListenAndServe(":8888", router))
 }
