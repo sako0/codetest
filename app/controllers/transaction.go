155b627e006b0a4e9989d24cfbc2f0a6f5e4b185
sako0 <y.m1911a1.0921@gmail.com>
2021-12-30 23:21:22 +0900

    test

--- app/controllers/transaction.go
+++ app/controllers/transaction.go
@@ -1,5 +1,7 @@
 package controllers
 
+// https://www.sunapro.com/mysql-lock/
+
 import (
 	"database/sql"
 	"encoding/json"
@@ -15,6 +17,13 @@ func (c Controller) AddTransaction(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var transaction models.Transaction
 		var errorObj models.Error
+
+		tx, err := db.Begin() // トランザクション開始
+		if err != nil {
+			errorObj.Message = "トランザクション開始できませんでした。"
+			utils.Respond(w, http.StatusBadRequest, errorObj)
+		}
+
 		json.NewDecoder(r.Body).Decode(&transaction)
 		if transaction.UserID < 0 {
 			errorObj.Message = "\"UserId\" が指定されていません"
@@ -31,8 +40,7 @@ func (c Controller) AddTransaction(db *sql.DB) http.HandlerFunc {
 			utils.Respond(w, http.StatusBadRequest, errorObj)
 			return
 		}
-
-		rows, err := db.Query("select * from transactions where user_id=?", transaction.UserID)
+		rows, err := tx.Query("select * from transactions where user_id=?", transaction.UserID)
 		if err != nil && err != sql.ErrNoRows {
 			log.Println(err)
 			errorObj.Message = "SQLエラーです"
@@ -42,6 +50,7 @@ func (c Controller) AddTransaction(db *sql.DB) http.HandlerFunc {
 		defer rows.Close()
 
 		totalAmount := 0
+		var lastId int = 0
 		for rows.Next() {
 			var id int
 			var user_id int
@@ -55,6 +64,7 @@ func (c Controller) AddTransaction(db *sql.DB) http.HandlerFunc {
 				return
 			}
 			totalAmount += amount
+			lastId = id
 		}
 		err = rows.Err()
 		if err != nil {
@@ -68,23 +78,36 @@ func (c Controller) AddTransaction(db *sql.DB) http.HandlerFunc {
 			utils.Respond(w, http.StatusPaymentRequired, errorObj)
 			return
 		}
-		result, err := db.Exec("INSERT INTO transactions (user_id, description, amount) values(?, ?, ?)", transaction.UserID, transaction.Description, transaction.Amount)
+		// https://blog.suganoo.net/entry/2019/01/25/190200
+		transaction.ID = lastId + 1
+		log.Println(transaction.ID)
+		result, err := tx.Exec("INSERT INTO transactions (id, user_id, description, amount) values(?, ?, ?, ?)", transaction.ID, transaction.UserID, transaction.Description, transaction.Amount)
 		if err != nil {
+			errorObj.Message = "execエラー"
 			log.Println(err)
-			errorObj.Message = "transactionの実行ができませんでした。"
-			utils.Respond(w, http.StatusInternalServerError, errorObj)
+			utils.Respond(w, http.StatusPaymentRequired, errorObj)
 			return
 		}
-		lastInsertID, err := result.LastInsertId()
-		if err != nil {
+		lastInsertID, insertErr := result.LastInsertId()
+		if insertErr != nil {
+			errorObj.Message = "lastInsertIdの取得エラーです"
 			log.Println(err)
-			errorObj.Message = "transactionのIDが取得できませんでした。"
-			utils.Respond(w, http.StatusInternalServerError, errorObj)
+			utils.Respond(w, http.StatusPaymentRequired, errorObj)
 			return
 		}
-		log.Println("lastInsertID=" + fmt.Sprint(lastInsertID) + ", totalAmount=" + fmt.Sprint(totalAmount))
-		errorObj.Message = "作成しました"
-		utils.Respond(w, http.StatusCreated, errorObj)
-
+		// https://sourjp.github.io/posts/go-db/
+		if lastId+1 != int(lastInsertID) {
+			tx.Rollback()
+			log.Println(err)
+			log.Println("lastInsertID=" + fmt.Sprint(lastInsertID) + ", totalAmount=" + fmt.Sprint(totalAmount) + ", last_id=" + fmt.Sprint(lastId))
+			errorObj.Message = "同時に複数回の登録はできません。"
+			utils.Respond(w, http.StatusPaymentRequired, errorObj)
+			return
+		} else {
+			tx.Commit()
+			log.Println("lastInsertID=" + fmt.Sprint(lastInsertID) + ", totalAmount=" + fmt.Sprint(totalAmount) + ", last_id=" + fmt.Sprint(lastId))
+			errorObj.Message = "作成しました"
+			utils.Respond(w, http.StatusCreated, errorObj)
+		}
 	}
 }
